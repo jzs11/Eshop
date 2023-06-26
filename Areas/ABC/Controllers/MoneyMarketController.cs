8750b3a5f50039a2d047a3bc387ff7b32086729d
Shan, Jing Zhe <JingZhe.Shan@fisglobal.com>
2023-06-27 09:22:23 +1200

    Add project files.

---
+++ Areas/ABC/Controllers/MoneyMarketController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eshop.Controllers
+{
+   [Area("ABC")]
+   public class MoneyMarketController : Controller
+   {
+      public ActionResult Index()
+      {
+         return View();
+      }
+   }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+using System.Text.RegularExpressions;
+
+namespace Eshop
+{
+   public class Program
+   {
+      public static void Main(string[] args)
+      {
+         var builder = WebApplication.CreateBuilder(args);
+
+         // Add services to the container.
+         builder.Services.AddRazorPages();
+         builder.Services.AddMvc();
+         builder.Services.AddRouting(options =>
+         {
+            options.ConstraintMap["slugify"] = typeof(SlugifyParameterTransformer);
+            options.LowercaseUrls = true;
+         });
+
+         var app = builder.Build();
+
+         // Configure the HTTP request pipeline.
+         if (!app.Environment.IsDevelopment())
+         {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+         }
+
+         app.UseHttpsRedirection();
+         app.UseStaticFiles();
+
+         app.UseRouting();
+
+         app.UseAuthorization();
+
+         app.MapRazorPages();
+
+
+         app.MapControllerRoute(
+             name: "area",
+             pattern: "{area:slugify}/{controller:slugify}/{action=Index}/{id?}");
+
+         app.MapControllerRoute(
+             name: "default",
+             pattern: "{controller:slugify}/{action=Index}/{id?}");
+
+
+         app.Run();
+      }
+   }
+
+   public class SlugifyParameterTransformer : IOutboundParameterTransformer
+   {
+      public string? TransformOutbound(object? value)
+      {
+         if (value == null) { return null; }
+
+         return Regex.Replace(value.ToString()!,
+                              "([a-z])([A-Z])",
+                              "$1-$2",
+                              RegexOptions.CultureInvariant,
+                              TimeSpan.FromMilliseconds(100)).ToLowerInvariant();
+      }
+   }
+}
